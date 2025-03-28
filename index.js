993d14dd7b270339ca1231ff4b6b82d35bd2b87c
rocky saputra <rocky.saputra@ifabula.com>
2025-03-28 22:33:48 +0700

    update index

--- index.js
+++ index.js
@@ -3,7 +3,7 @@ const express = require('express')
 const app = express()
 
 app.get('/', (req, res) => {
-    res.send('Hello, CI/CD world! 👋');
+    res.send('Desy istri kuuu ❤❤❤');
   });
   
 app.get('/health', (req, res) => {
